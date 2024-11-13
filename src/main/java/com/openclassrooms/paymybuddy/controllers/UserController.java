12066f60055eedce1607f1dc13dc0bf4237b899c
QuentinTL <q.thibaut.lazaro96@gmail.com>
2024-11-14 00:30:38 +0100

    Create UserDo to treated the client informations Create UserController to look and treated the request and vue

---
+++ src/main/java/com/openclassrooms/paymybuddy/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.openclassrooms.paymybuddy.controllers;
+
+import com.openclassrooms.paymybuddy.dto.UserDto;
+import com.openclassrooms.paymybuddy.models.User;
+import com.openclassrooms.paymybuddy.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+        User user = userService.create(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> modifyUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+        User user = userService.modify(id, userDto);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        try {
+            userService.delete(id);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+}

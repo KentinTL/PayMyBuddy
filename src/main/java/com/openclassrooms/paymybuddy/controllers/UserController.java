dca99ecddb92e1506b4da63bf928cb9ff262a0cc
QuentinTL <q.thibaut.lazaro96@gmail.com>
2024-12-11 22:25:25 +0100

    added route, logic and front to add relation

--- src/main/java/com/openclassrooms/paymybuddy/controllers/UserController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controllers/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/users")
@@ -43,5 +45,37 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.findAll();
+    }
+
+    @PostMapping("/{userId}/relations")
+    public String addRelation(@PathVariable Long userId, @RequestParam String friendEmail, Model model) {
+        try {
+            userService.addRelation(userId, friendEmail);
+            model.addAttribute("successMessage", "Relation ajoutée avec succès !");
+        } catch (RuntimeException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+
+        Set<User> relations = userService.getRelations(userId);
+        model.addAttribute("relations", relations);
+
+        model.addAttribute("userId", userId);
+
+        return "relation";
+    }
+
+    @GetMapping("/{userId}/relations")
+    public String getRelations(@PathVariable Long userId, Model model) {
+        try {
+            Set<User> relations = userService.getRelations(userId);
+            model.addAttribute("relations", relations);
+            model.addAttribute("userId", userId);
+        } catch (RuntimeException e) {
+            model.addAttribute("errorMessage", "Impossible de récupérer les relations.");
+        }
+
+        model.addAttribute("userId", userId);
+
+        return "relation";
     }
 }

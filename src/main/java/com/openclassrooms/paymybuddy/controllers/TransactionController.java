3ba75e64a1109bc614da8c4b365950b4fb8ea997
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-01-10 18:12:16 +0100

    change view params

--- src/main/java/com/openclassrooms/paymybuddy/controllers/TransactionController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controllers/TransactionController.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.paymybuddy.controllers;
 
 import com.openclassrooms.paymybuddy.dto.TransferDto;
+import com.openclassrooms.paymybuddy.services.AccountBalanceService;
 import com.openclassrooms.paymybuddy.services.TransactionService;
 import com.openclassrooms.paymybuddy.services.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @Controller
 @RequestMapping("/transaction")
 public class TransactionController {
@@ -26,8 +29,9 @@ public class TransactionController {
         try {
             // Charger les relations et les transactions
             model.addAttribute("relations", userService.getRelations(userId));
-            model.addAttribute("transaction", transactionService.getTransactionsByUserId(userId));
+            model.addAttribute("transactions", transactionService.getTransactionsByUserId(userId));
             model.addAttribute("userId", userId);
+            model.addAttribute("transferDo", new TransferDto());
         } catch (RuntimeException e) {
             model.addAttribute("errorMessage", "Erreur lors du chargement des données : " + e.getMessage());
         }
@@ -35,7 +39,7 @@ public class TransactionController {
     }
 
     @PostMapping
-    public String makeTransaction(@Valid @RequestBody TransferDto transferDto, Model model) {
+    public String makeTransaction(@Valid @ModelAttribute("transferDto") TransferDto transferDto, Model model) {
         try {
             transactionService.makeTransfer(transferDto);
             model.addAttribute("successMessage", "Transaction effectuée avec succès !");
@@ -45,7 +49,7 @@ public class TransactionController {
 
         // Recharger les données pour l'affichage
         model.addAttribute("relations", userService.getRelations(transferDto.getUserId()));
-        model.addAttribute("transaction", transactionService.getTransactionsByUserId(transferDto.getUserId()));
+        model.addAttribute("transactions", transactionService.getTransactionsByUserId(transferDto.getUserId()));
         model.addAttribute("userId", transferDto.getUserId());
 
         return "transaction";

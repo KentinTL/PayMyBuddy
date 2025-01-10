7528d80c5dee7f5ad19e1f5b41fcad74233cf28c
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-01-10 18:12:52 +0100

    Add regex to params

--- src/main/java/com/openclassrooms/paymybuddy/dto/TransferDto.java
+++ src/main/java/com/openclassrooms/paymybuddy/dto/TransferDto.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.paymybuddy.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +25,9 @@ public class TransferDto {
     private Long receiverId;
 
     @NotNull
+    @Min(value = 1, message = "Le montant doit être supérieur ou égal à 1.")
     private BigDecimal amount;
 
+    @Size(max = 255, message = "La description ne peut pas dépasser 255 caractères.")
     private String description;
 }

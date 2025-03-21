6449c1a06bb21561123af6271d1bcbf9972c2484
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-03-21 11:43:38 +0100

    Delete unnecessary class and service by using spring security

--- src/main/java/com/openclassrooms/paymybuddy/services/IAuthService.java
+++
@@ -1,7 +0,0 @@
-package com.openclassrooms.paymybuddy.services;
-
-import com.openclassrooms.paymybuddy.models.User;
-
-public interface IAuthService {
-    public User auth(String email, String password);
-}

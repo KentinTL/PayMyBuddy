0f073b9908ec7051941f662b01c8b16e7b99107f
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-03-21 11:48:33 +0100

    Create new DTOs class adding more practicity for the logic

---
+++ src/main/java/com/openclassrooms/paymybuddy/dto/WithdrawalDto.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.paymybuddy.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WithdrawalDto {
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    @Min(value = 1, message = "Le montant doit être supérieur ou égal à 1.")
+    private BigDecimal amount;
+
+    @Size(max = 255, message = "La description ne peut pas dépasser 255 caractères.")
+    private String description;
+}

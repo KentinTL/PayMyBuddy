de75a529b0242c22e4a78c73225bd0157ca5cdbc
QuentinTL <q.thibaut.lazaro96@gmail.com>
2025-03-26 00:06:41 +0100

    Modified PercentageCalculator to optimize the code for future changes

--- src/main/java/com/openclassrooms/paymybuddy/tools/PercentageCalculator.java
+++ src/main/java/com/openclassrooms/paymybuddy/tools/PercentageCalculator.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.paymybuddy.tools;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -7,13 +8,15 @@ import java.math.RoundingMode;
 
 @Component
 public class PercentageCalculator {
+    @Value("${commission.pourcentage}")
+    private String discountPercentage;
 
     public BigDecimal calculPercent(BigDecimal amount) {
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Le montant doit être positif.");
         }
 
-        BigDecimal percentageValue = amount.multiply(BigDecimal.valueOf(0.005)); // 0.5%
-        return percentageValue.setScale(2, RoundingMode.HALF_UP); // Arrondi à 2 décimales
+        BigDecimal percentageValue = amount.multiply(BigDecimal.valueOf(Double.parseDouble(discountPercentage)));
+        return percentageValue.setScale(2, RoundingMode.HALF_UP);
     }
 }

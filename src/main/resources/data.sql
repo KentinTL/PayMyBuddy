b22e9503e127b0c54a61018d929f6b664e737697
QuentinTL <48092230+KentinTL@users.noreply.github.com>
2025-04-01 11:51:00 +0200

    remove data.sql from src/main/ressources/static/sql folder to ressources because we have only one sql file so we dont need a sql folder Added CREATE DATABASE IF NOT EXISTS paymybuddy so we can launch the app if the db is not already create

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
--- Creation de la table "users"
-CREATE TABLE users (
+CREATE DATABASE IF NOT EXISTS paymybuddy;
+USE paymybuddy;
+
+CREATE TABLE IF NOT EXISTS users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL
 );
 
--- Creation de la table "user_friends"
-CREATE TABLE user_friends (
+CREATE TABLE IF NOT EXISTS user_friends (
     user_id BIGINT NOT NULL,
     friend_id BIGINT NOT NULL,
     PRIMARY KEY (user_id, friend_id),
@@ -15,14 +16,13 @@ CREATE TABLE user_friends (
     FOREIGN KEY (friend_id) REFERENCES users(id)
 );
 
--- Creation de la table "transactions"
-CREATE TABLE transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     amount DECIMAL(38,2) NOT NULL,
     date DATETIME(6) NOT NULL,
     description VARCHAR(255),
     fee DECIMAL(38,2) NOT NULL,
-    transaction_type TINYINT NOT NULL,
+    transaction_type VARCHAR(255) NOT NULL,
     receiver_id BIGINT NOT NULL,
     sender_id BIGINT NOT NULL,
     FOREIGN KEY (receiver_id) REFERENCES users(id),
